72a28b1ef22a5a83dfa949a4762567f547e2e1ea
Anikcse19 <anikdebcse@gmail.com>
2024-10-05 02:35:33 +0600

    add global tyepe

---
+++ src/type.ts
@@ -0,0 +1,30 @@
+export interface UserData {
+  _id?: string;
+  name: string;
+  user_name: string;
+  father_or_husband_name: string;
+  mother_name: string;
+  picture: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: string;
+  present_address: string;
+  permanent_address: string;
+  nationality: string;
+  religion: string;
+  blood_group: string;
+  nid_passport_no: string;
+  dob: string;
+  choice_side: string;
+  marital_status: string;
+  profession: string;
+  reference_id: string;
+  parent_placement_id: string;
+  nominee_name: string;
+  relation_with_nominee: string;
+  nominee_address: string;
+  nominee_mobile_no: string;
+  nominee_picture: string;
+  registration_date: string;
+}
