c00f1203bc5276d51af9036da69f9baa6d58a1be
Anikcse19 <anikdebcse@gmail.com>
2024-10-29 12:02:17 +0600

    navbar-fixed

--- src/components/Navbar-Menu.tsx
+++ src/components/Navbar-Menu.tsx
@@ -21,7 +21,10 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-xl mx-auto  z-50", className)}
+      className={cn(
+        "fixed top-10 inset-x-0 max-w-2xl mx-auto  z-50",
+        className
+      )}
     >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
