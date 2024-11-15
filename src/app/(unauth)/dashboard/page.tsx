35d47e0fb32f5ea991613b1fc136678dd04ad7cb
Anikcse19 <anikdebcse@gmail.com>
2024-11-15 23:40:53 +0600

    dash home updated

--- src/app/(unauth)/dashboard/page.tsx
+++ src/app/(unauth)/dashboard/page.tsx
@@ -15,6 +15,8 @@ import ShareHolderChart from "@/components/Charts/ShareHolderChart";
 import useStore from "../../../Zustand/Store/userStore";
 import baseUrl from "../../../../config";
 import ClubBonusChart from "@/components/Charts/ClubBonusChart";
+import BonusChart from "@/components/Charts/BonusChart";
+import IncomeWalletPieChart from "@/components/Charts/IncomeWalletPieChart";
 
 const Dashboard = () => {
   const [user, setUser] = useState<any>({});
@@ -53,261 +55,101 @@ const Dashboard = () => {
   return (
     <div className="w-full h-full">
       <div className=" flex flex-col gap-y-10">
-        {/* name intro, my invest,share profit */}
+        {/* all wallet*/}
         <div className="flex flex-wrap items-center  gap-5">
           <div className="w-full flex items-center justify-between gap-x-4">
-            {/* name intro */}
-            <div className="w-[60%] h-60  rounded-md p-5 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-              <div className="flex gap-x-6 h-full">
-                <div className="w-[50%] flex flex-col gap-y-7 ">
-                  <div className="flex gap-x-3">
-                    <img
-                      className="w-20 h-20"
-                      src="/images/profilePicIcon.png"
-                      alt=""
-                    />
-                    <span className="flex self-center flex-col gap-y-2">
-                      <p className="text-black font-bold">Welcome Back</p>
-                      <p className="text-slate-600">{user && user?.name}</p>
-                    </span>
-                  </div>
-
-                  <div className="flex items-center gap-6 ml-20 ">
-                    <div className="text-black  flex flex-col gap-y-2">
-                      <p>
-                        &#x9F3;
-                        {user?.wallet?.income_wallet
-                          ? Math.ceil(user?.wallet?.income_wallet)
-                          : 0}
-                      </p>
-                      <p>Income Wallet</p>
-                      <p className="w-[60px] h-2 rounded-md bg-green-600" />
-                    </div>
-                    <div className="text-black flex flex-col gap-y-1">
-                      <p>
-                        &#x9F3;{" "}
-                        {user?.wallet?.purchase_wallet
-                          ? Math.ceil(user?.wallet?.purchase_wallet)
-                          : 0}
-                      </p>
-                      <p>Purchase Wallet</p>
-                      <p className="w-[80px] h-2 rounded-md bg-red-700" />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-[50%] h-full flex justify-center items-center">
-                  <img
-                    className="w-[70%]"
-                    src="/images/dashboardImg1.png"
-                    alt=""
-                  />
-                </div>
+            <div className="w-full h-40  rounded-md  flex items-center gap-4">
+              {/* income wallet */}
+              <div className="h-full w-full bg-purple-200 flex flex-col gap-y-2 justify-center px-6">
+                <p className="text-3xl text-purple-600">
+                  &#x9F3; {Math.ceil(user?.wallet?.income_wallet)}
+                </p>
+                <p>Income Wallet</p>
               </div>
-              <div></div>
-            </div>
-
-            {/* invest part */}
-            <div className="w-[40%] h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-              <div className="h-full flex flex-col justify-between gap-y-2">
-                <div className=" flex flex-col justify-between">
-                  <p className="font-bold">&#x9F3; 0.00</p>
-                  <p className="text-rose-700 font-bold">My Invest</p>
-                </div>
-                <div className=" flex justify-center items-center ">
-                  <div className="w-[100px] h-[100px] rounded-full border-4 border-yellow-500 flex justify-center items-center p-1">
-                    <div className="text-center">
-                      <p className="text-sm">Total Orders</p>
-                      <p className="text-sm">21%</p>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <p className="inline font-bold">&#x9F3; 0.00</p>
-                  <p className="inline ml-3">from last month</p>
-                </div>
+              {/* project share wallet */}
+              <div className="h-full w-full bg-blue-200 flex flex-col gap-y-2 justify-center px-6">
+                <p className="text-3xl text-blue-600">
+                  &#x9F3;{" "}
+                  {Math.ceil(
+                    user?.wallet?.project_share
+                      ? user?.wallet?.project_share
+                      : 0
+                  )}
+                </p>
+                <p>Project Share Wallet</p>
               </div>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-between w-full gap-10">
-            <div className="w-full flex items-center gap-10">
-              {/*share profit part */}
-              <div className="w-full h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-                <div className="h-full flex flex-col justify-between gap-y-2">
-                  <div className=" flex flex-col justify-between">
-                    <p className="font-bold">&#x9F3; 0.00</p>
-                    <p className="text-rose-700 font-bold">Share Profit</p>
-                  </div>
-                  <div className="w-full h-40 flex justify-center items-center">
-                    <ShareProfitChart />
-                  </div>
-                  <div>
-                    <p className="inline font-bold">&#x9F3; 0.00</p>
-                    <p className="inline ml-3">from last month</p>
-                  </div>
-                </div>
+              {/*fixed deposite wallet */}
+              <div className="h-full w-full bg-green-200 flex flex-col gap-y-2 justify-center px-6">
+                <p className="text-3xl text-green-600">
+                  &#x9F3;{" "}
+                  {Math.ceil(
+                    user?.wallet?.fixed_deposit
+                      ? user?.wallet?.fixed_deposit
+                      : 0
+                  )}
+                </p>
+                <p>Fixed Deposite Wallet</p>
               </div>
-              {/* fixed deposit part */}
-              <div className="w-full h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-                <div className="h-full flex flex-col justify-between gap-y-2">
-                  <div className=" flex flex-col justify-between">
-                    <p className="font-bold">&#x9F3; 0.00</p>
-                    <p className="text-rose-700 font-bold">Fixed Deposit</p>
-                  </div>
-                  <div className="w-full h-40 flex justify-center items-center">
-                    <FixedDepositChart />
-                  </div>
-                  <div>
-                    <p className="inline font-bold">&#x9F3; 0.00</p>
-                    <p className="inline ml-3">from last month</p>
-                  </div>
-                </div>
+              {/*  share holder wallet */}
+              <div className="h-full w-full bg-yellow-200 flex flex-col gap-y-2 justify-center px-6">
+                <p className="text-3xl text-yellow-600">
+                  &#x9F3;{" "}
+                  {Math.ceil(
+                    user?.wallet?.share_holder ? user?.wallet?.share_holder : 0
+                  )}
+                </p>
+                <p>Share Holder Wallet</p>
               </div>
-            </div>
-
-            <div className="w-full flex items-center gap-10">
-              {/* share holder part */}
-              <div className="w-full h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-                <div className="h-full flex flex-col justify-between gap-y-2">
-                  <div className=" flex flex-col justify-between">
-                    <p className="font-bold">&#x9F3; 0.00</p>
-                    <p className="text-rose-700 font-bold">Share Holder</p>
-                  </div>
-                  <div className="w-full h-40 flex justify-center items-center">
-                    <ShareHolderChart />
-                  </div>
-                  <div>
-                    <p className="inline font-bold">&#x9F3; 0.00</p>
-                    <p className="inline ml-3">from last month</p>
-                  </div>
-                </div>
-              </div>
-              {/* directorship part */}
-              <div className="w-full h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-                <div className="h-full flex flex-col justify-between gap-y-2">
-                  <div className=" flex flex-col justify-between">
-                    <p className="font-bold">&#x9F3; 0.00</p>
-                    <p className="text-rose-700 font-bold">Directorship</p>
-                  </div>
-                  <div className="w-full h-40 flex justify-center items-center">
-                    <ShareProfitChart />
-                  </div>
-                  <div>
-                    <p className="inline font-bold">&#x9F3; 0.00</p>
-                    <p className="inline ml-3">from last month</p>
-                  </div>
-                </div>
+              {/* partnership wallet */}
+              <div className="h-full w-full bg-red-200 flex flex-col gap-y-2 justify-center px-6">
+                <p className="text-3xl text-red-600">
+                  &#x9F3;{" "}
+                  {Math.ceil(
+                    user?.wallet?.directorship ? user?.wallet?.directorship : 0
+                  )}
+                </p>
+                <p>Partnership Wallet</p>
               </div>
             </div>
           </div>
         </div>
 
-        {/* referrel bonus , matching bonus and get in touch  */}
-
-        <div className="flex items-center gap-2">
-          {/* referrel bonus , matching bonus */}
-          <div className="w-[60%]">
-            <div className="w-full flex items-center gap-3">
-              {/* referrel bonus */}
-              <div className="w-full h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-                <div className="flex justify-between">
-                  <div>
-                    <p className="font-bold">&#x9F3; 0.00</p>
-                    <p className="text-rose-700 font-bold">Refferel Bonus</p>
-                  </div>
-                  <div className="px-2 h-8 bg-green-700 shadow-lg cursor-pointer rounded-sm flex justify-center items-center">
-                    <p className="text-white text-sm font-bold">View</p>
-                  </div>
-                </div>
-                <div className="w-full h-28 mt-6">
-                  <RefferelBonusChart />
-                </div>
-              </div>
-              {/* team bonus */}
-              <div className="w-full h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-                <div className="flex justify-between">
-                  <div>
-                    <p className="font-bold">&#x9F3; 0.00</p>
-                    <p className="text-rose-700 font-bold">Team Bonus</p>
-                  </div>
-                  <div className="px-2 h-8 bg-green-700 shadow-lg rounded-sm cursor-pointer flex justify-center items-center">
-                    <p className="text-white text-sm font-bold">View</p>
-                  </div>
-                </div>
-                <div className="w-full h-28 mt-6">
-                  <MatchingBonusChart />
-                </div>
-              </div>
-            </div>
+        {/* referrel bonus , matching bonus and club bonus  */}
+        <div className="bg-white w-full h-fit p-3">
+          <p className="text-2xl border-b-2 mb-2 border-slate-700">Bonus</p>
+          <div className="flex gap-x-9 py-4">
+            <p>
+              Reference &#x9F3;{" "}
+              {Math.ceil(
+                user?.wallet?.reference_bonus
+                  ? user?.wallet?.reference_bonus
+                  : 0
+              )}
+            </p>
+            <p>
+              Team &#x9F3;{" "}
+              {Math.ceil(
+                user?.wallet?.matching_bonus ? user?.wallet?.matching_bonus : 0
+              )}
+            </p>
+            <p>
+              Club &#x9F3;{" "}
+              {Math.ceil(
+                user?.wallet?.club_bonus ? user?.wallet?.club_bonus : 0
+              )}
+            </p>
           </div>
-
-          {/* club bonus */}
-
-          <div className="w-[40%] h-60  p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-            <div className="flex justify-between">
-              <div>
-                <p className="font-bold">&#x9F3; 0.00</p>
-                <p className="text-rose-700 font-bold">Club Bonus</p>
-              </div>
-              <div className="px-2 h-8 bg-green-700 shadow-lg rounded-sm cursor-pointer flex justify-center items-center">
-                <p className="text-white text-sm font-bold">View</p>
-              </div>
-            </div>
-            <div className="w-full h-28 mt-6">
-              <ClubBonusChart />
-            </div>
+          {/* graph */}
+          <div className="p-3 py-8 flex items-center justify-between">
+            <BonusChart />
+            <IncomeWalletPieChart />
           </div>
-          {/*  get in touch */}
-          {/* <div className="w-[40%] h-60 p-5 rounded-md text-slate-700 hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer">
-            <div>
-              <p className="text-rose-700 font-bold text-lg">Get in touch</p>
-              <div className="mt-10 flex justify-center items-center">
-                <div className="flex items-center gap-3">
-                  <FaFacebook
-                    style={{
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-                    }}
-                    className="text-4xl text-blue-700 rounded-full cursor-pointer  hover:scale-110 transition-all duration-300 ease-in"
-                  />
-                  <FaTwitter
-                    style={{
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-                    }}
-                    className="text-4xl bg-blue-500 p-2 rounded-full text-white cursor-pointer  hover:scale-110 transition-all duration-300 ease-in"
-                  />
-                  <FaTelegram
-                    style={{
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-                    }}
-                    className="text-4xl rounded-full text-blue-500 cursor-pointer  hover:scale-110 transition-all duration-300 ease-in"
-                  />
-                  <IoLogoYoutube
-                    style={{
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-                    }}
-                    className="text-4xl bg-red-500 p-1.5 rounded-full text-white cursor-pointer  hover:scale-110 transition-all duration-300 ease-in"
-                  />
-                  <IoMail
-                    style={{
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-                    }}
-                    className="text-4xl bg-red-600 p-2 rounded-full text-white cursor-pointer  hover:scale-110 transition-all duration-300 ease-in"
-                  />
-                  <RiWhatsappLine
-                    style={{
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-                    }}
-                    className="text-4xl bg-green-500 p-1 rounded-full text-white cursor-pointer  hover:scale-110 transition-all duration-300 ease-in"
-                  />
-                </div>
-              </div>
-            </div>
-          </div> */}
+        </div>
+
+        {/* purchase_wallet */}
+
+        <div className=" w-full flex p-3">
+          <div className="h-fit bg-white p-5">hello</div>
         </div>
       </div>
     </div>
