a1ecb7bf375648318825926bc764cdd00e4499cf
Anikcse19 <anikdebcse@gmail.com>
2025-02-05 12:36:38 +0600

    link up with dummy database

--- config.ts
+++ config.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const baseUrl = "https://bondhu-group-dev-server.onrender.com/api/v1";
-=======
-// const baseUrl = "https://bondhu-group-server.onrender.com/api/v1";
 
-const baseUrl = "https://bondhu-group-server.onrender.com/api/v5";
->>>>>>> 6158a114a7c8d0386a40f5791259bfcaaad502a1
+// const baseUrl = "https://bondhu-group-server.onrender.com/api/v5";
 
 export default baseUrl;
